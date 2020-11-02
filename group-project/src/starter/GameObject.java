90a7c2635350362b9c2754e42fe9d6e57759a30d
Jefferson Pelera <j_pelera1@u.pacific.edu>
2020-11-02 09:55:35 -0800

    removed velocityX and velocityY

--- group-project/src/starter/GameObject.java
+++ group-project/src/starter/GameObject.java
@@ -2,15 +2,12 @@ package starter;
 
 public abstract class GameObject {
 
-	private double x, y, velocityX, velocityY;
+	private double x, y;
 	private int width, height;
-	public GameObject(double x, double y, 
-					  double velocityX, double velocityY,
+	public GameObject(double x, double y,
 					  int width, int height) {
 		this.x = x;
 		this.y = y;
-		this.velocityX =  velocityX;
-		this.velocityY =  velocityY;
 		this.width = width;
 		this.height = height;
 	}
@@ -23,12 +20,6 @@ public abstract class GameObject {
 	}
 	public void setY(double y) {
 		this.y = y;
-	}
-	public void setVelocityX(double velocityX) {
-		this.velocityX = velocityX;
-	}
-	public void setVelocityY(double velocityY) {
-		this.velocityY = velocityY;
 	}
 	public void setWidth(int width) {
 		this.width = width;
@@ -45,12 +36,6 @@ public abstract class GameObject {
 	}
 	public double getY() {
 		return y;
-	}
-	public double getVelocityX() {
-		return velocityX;
-	}
-	public double getVelocityY() {
-		return velocityY;
 	}
 	public int getWidth() {
 		return width;
