12738cde64e31c12d220e1013a0d0cc6d121c4c1
Benjamin Ayers <b_ayers@u.pacific.edu>
2020-11-04 10:38:18 -0800

    Created constructor, getters, and setters for Player class

--- group-project/src/starter/Player.java
+++ group-project/src/starter/Player.java
@@ -1,9 +1,41 @@
 package starter;
 
-public class Player {
+public class Player extends GameObject{
 
-	public Player() {
-		// TODO Auto-generated constructor stub
+	private PlayerType type;
+	
+	public Player(double x, double y, int width, int height, PlayerType type) {
+		super(x, y, width, height);
+		this.type = type;
+		
+	}
+
+	public PlayerType getType() {
+		return type;
+	}
+
+	public void setType(PlayerType type) {
+		this.type = type;
+	}
+
+	public void createPlayer() {
+		
+	}
+	
+	public boolean move() {
+		return true;
+	}
+
+	public boolean OutOfBounds() {
+		return true;
+	}
+	
+	public boolean collision() {
+		return true;
+	}
+	
+	public void bounds() {
+		
 	}
 
 }
