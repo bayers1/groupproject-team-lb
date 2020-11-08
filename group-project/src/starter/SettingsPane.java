958d354e5347111e46a10144d53b9bc17164d65c
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2020-11-07 20:07:25 -0500

    added few things to the settings pane.

--- group-project/src/starter/SettingsPane.java
+++ group-project/src/starter/SettingsPane.java
@@ -2,28 +2,52 @@ package starter;
 
 import java.awt.event.MouseEvent;
 import java.awt.Color;
+import java.awt.Font;
 
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
 public class SettingsPane extends GraphicsPane {
 	private MainApplication program;
 	private GButton Back;
-	
+	private GLabel soundLabel;
+	private GLabel musicLabel;
+	private GButton OnOff;
+	private GButton  OnOff1;
 	public SettingsPane(MainApplication app) {
 		super();
 		Back = new GButton("Back", LEFT_BOTTOM, BOTTOM, REG_BUTTON_WIDTH, REG_BUTTON_HEIGHT);
 		program = app;
+		soundLabel = new GLabel("Sound",150,200);
+		soundLabel.setFont(new Font("Ariel", Font.PLAIN,25));
+		soundLabel.setColor(Color.black);
+		musicLabel = new GLabel("Music",150,400);
+		musicLabel.setFont(new Font("Ariel", Font.PLAIN,25));
+		musicLabel.setColor(Color.black);
+		OnOff = new GButton("ON/OFF",800,165,50,50);
+		OnOff.setColor(Color.BLUE);
+		OnOff1 =new GButton("ON/OFF",800,365,50,50);
+		OnOff1.setColor(Color.BLUE);
+	
 	}
 		
 	@Override
 	public void showContents() {
 		program.add(Back);
+		program.add(soundLabel);
+		program.add(musicLabel);
+		program.add(OnOff);
+		program.add(OnOff1);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(Back);
+		program.remove(soundLabel);
+		program.remove(musicLabel);
+		program.remove(OnOff);
+		program.remove(OnOff1);
 	}
 
 	@Override
@@ -31,6 +55,12 @@ public class SettingsPane extends GraphicsPane {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if (obj == Back) {
 			program.switchToMenu();
+		}
+		else if(obj == OnOff) {
+		
+		}
+		else if(obj == OnOff1) {
+			
 		}
 	}
 	}
