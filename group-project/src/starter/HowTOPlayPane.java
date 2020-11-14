084cc99aedd6538d535e9934bb03872b4f365f1d
Jefferson Pelera <j_pelera1@u.pacific.edu>
2020-11-14 15:39:45 -0800

    added pane pictures

--- group-project/src/starter/HowTOPlayPane.java
+++ group-project/src/starter/HowTOPlayPane.java
@@ -2,6 +2,8 @@ package starter;
 
 import java.awt.event.MouseEvent;
 import java.awt.Font;
+
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
@@ -10,21 +12,27 @@ public class HowTOPlayPane extends GraphicsPane {
 	private GButton Back;
 	public static final int START_X = 100;
 	public static final int START_Y = 200;
+	public static final int PANE_X = 320;
+	public static final int PANE_Y = 160;
 	
+	private GImage pane;
 	public HowTOPlayPane(MainApplication app) {
 		super();
 		program = app;
 		Back = new GButton("Back", LEFT_BOTTOM, BOTTOM, REG_BUTTON_WIDTH, REG_BUTTON_HEIGHT);
+		pane = new GImage("pane1.png", PANE_X, PANE_Y);
 	}
 		
 	@Override
 	public void showContents() {
 		program.add(Back);
+		program.add(pane);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(Back);
+		program.removeAll();
+		//program.remove(Back);
 	}
 
 	@Override
