c132b8a4d806cb9f7e347e9cbae372f517cea66a
Benjamin Ayers <b_ayers@u.pacific.edu>
2020-11-16 10:03:14 -0800

    ObstacleType
    Created various types for ObstacleType, one for each elements' floating
    obstacles and one for pillar-like obstacles

--- group-project/src/starter/ObstacleType.java
+++ group-project/src/starter/ObstacleType.java
@@ -1,9 +1,28 @@
 package starter;
 
-public class ObstacleType {
-
-	public ObstacleType() {
-		// TODO Auto-generated constructor stub
+public enum ObstacleType {
+	FIRE1, FIRE2, WATER1, WATER2, EARTH1, EARTH2, AIR1, AIR2;
+	
+	public String toString() {
+		switch(this) {
+			case FIRE1: 
+				return "fire1";
+			case FIRE2:
+				return "fire2";
+			case WATER1:
+				return "water1";
+			case WATER2:
+				return "water2";
+			case EARTH1:
+				return "earth1";
+			case EARTH2:
+				return "earth2";
+			case AIR1:
+				return "air1";
+			case AIR2:
+				return "air2";
+			}
+		return "N/A";
 	}
 
 }
