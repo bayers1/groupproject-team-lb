1ee8af7f5878c8ea2f8327792483135ea50a672e
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2020-11-19 16:19:47 -0500

    highlighting of menu buttons when mouse moved.

--- group-project/src/starter/MenuPane.java
+++ group-project/src/starter/MenuPane.java
@@ -2,6 +2,7 @@ package starter;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import acm.graphics.GImage;
 import acm.graphics.GObject;
@@ -16,23 +17,31 @@ public class MenuPane extends GraphicsPane {
 	private GButton play;
 	private GButton settings;
 	private GButton howToPlay;
-	
+	GButton button;
+	private ArrayList<GButton> gButtons = new ArrayList<GButton>();
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		
+		/*
+		for (int i = 0; i < 6; i++) {
+			GButton button = new GButton(gbuttonStrings[i], 280, 235 + 55 * i, MainApplication.BUTTON_HEIGHT,
+					MainApplication.BUTTON_WIDTH);
+			button.setFillColor(Color.GREEN);
+			gButtons.add(button);
+		}
+		*/
 		//creating the main buttons for the menu
 		play = new GButton("   Play   ", CENTER - (MENU_BUTTON_WIDTH/2), 
 						   START_POS, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
-		play.setFillColor(Color.GRAY);
+		play.setFillColor(Color.cyan);
 		
 		settings = new GButton("Settings",CENTER - (MENU_BUTTON_WIDTH/2),
 							   START_POS+MENU_BUTTON_HEIGHT+REG_PADDING, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
-		settings.setFillColor(Color.GRAY);
+		settings.setFillColor(Color.cyan);
 		
 		howToPlay = new GButton("How to Play",CENTER - (MENU_BUTTON_WIDTH/2),
 								START_POS+(2*MENU_BUTTON_HEIGHT)+(2*REG_PADDING), MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
-		howToPlay.setFillColor(Color.GRAY);
+		howToPlay.setFillColor(Color.cyan);
 	}
 
 	@Override
@@ -60,5 +69,29 @@ public class MenuPane extends GraphicsPane {
 		else if (obj == howToPlay) {
 			program.switchToHowToPlay();
 		}
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		Color highlightColor = new Color(0,220,0);
+		boolean something = true;
+		if(something) {
+			play.setFillColor(Color.cyan);
+			settings.setFillColor(Color.cyan);
+			howToPlay.setFillColor(Color.cyan);
+		}
+			if(obj == play) {
+			play.setFillColor(highlightColor);
+			something = false;
+		}
+			else if(obj == settings) {
+			settings.setFillColor(highlightColor);
+			something = false;
+		}
+			else if(obj == howToPlay) {
+			howToPlay.setFillColor(highlightColor);
+			something = false;
+		}
+	
 	}
 }
