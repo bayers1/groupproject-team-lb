a05b38d36565b4e248cc9b9f6d2034ff2401e3f4
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2020-11-30 18:47:52 -0500

    removed stuff because of redundant code.

--- group-project/src/starter/SettingsPane.java
+++ group-project/src/starter/SettingsPane.java
@@ -15,10 +15,6 @@ public class SettingsPane extends GraphicsPane {
 	private GLabel soundLabel, musicLabel,soundStatus,musicStatus;
 	private ArrayList<GButton> sButtons = new ArrayList<GButton>();
 	private ArrayList<GLabel> slabels = new ArrayList<GLabel>();
-	private GRect textBox = new GRect(280, 450, 570, 40);
-	private GLabel statusLabel = new GLabel("Sound and Music are always on by default.", textBox.getX() + 20, textBox.getY() + 25);
-	private boolean isSoundOff = false;
-	private boolean isMusicOff = false;
 	private GLabel settings = new GLabel("S E T T I N G S",430, 100);
 	
 	public SettingsPane(MainApplication app) {
@@ -49,9 +45,6 @@ public class SettingsPane extends GraphicsPane {
 		for(GLabel label:labels) {
 			slabels.add(label);
 		}
-		textBox.setFillColor(Color.yellow);
-		textBox.setFilled(true);
-		statusLabel.setFont(new Font("Ariel", Font.PLAIN,25));
 		settings.setFont(new Font("Ariel", Font.PLAIN,40));
 	}
 	@Override
@@ -62,8 +55,6 @@ public class SettingsPane extends GraphicsPane {
 		for(GLabel label:slabels) {
 			program.add(label);
 		}
-		program.add(textBox);
-		program.add(statusLabel);
 		program.add(settings);
 	}
 
@@ -83,12 +74,10 @@ public class SettingsPane extends GraphicsPane {
 				program.stopSound(program.getSoundFiles()[3]);
 				program.setSound(false);
 				slabels.get(2).setLabel("OFF");
-				isSoundOff = true;	
 			}
 			else {
 				program.setSound(true);
 				slabels.get(2).setLabel("ON");
-				isSoundOff = false;
 			}
 		
 		}
@@ -97,13 +86,11 @@ public class SettingsPane extends GraphicsPane {
 				program.stopMusic(program.getSoundFiles()[2]);
 				program.setMusic(false);
 				slabels.get(3).setLabel("OFF");
-				isMusicOff = true;
 			}
 			else {
 				program.setMusic(true);
 				program.playMusic(program.getSoundFiles()[2],true);
 				slabels.get(3).setLabel("ON");
-				isMusicOff = false;
 			}
 			
 		}
@@ -118,18 +105,5 @@ public class SettingsPane extends GraphicsPane {
 			hover(sButtons.get(i));
 			}
 	}
-		if(obj == null) {
-			statusLabel.setLabel("Sound and Music are always on by default.");
-		}
-		if(obj == sButtons.get(1) || obj == slabels.get(2)) {
-			if(isSoundOff)statusLabel.setLabel("Game Sound is currently off.");
-			else statusLabel.setLabel("Game Sound is currently on.");
-		}
-	   if(obj == sButtons.get(2) || obj == slabels.get(3)) {
-		   	if(isMusicOff)statusLabel.setLabel("Game Music is currently off.");
-			else statusLabel.setLabel("Game Music is currently on.");
-		}
 	}
-
-	
 }
