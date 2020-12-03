c188eaffe467035e263fddfaa1426d369300cd1c
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2020-12-02 23:14:42 -0500

    worked on gameover pane.Displays current score and top 5 scores.

--- group-project/src/starter/GameOverPane.java
+++ group-project/src/starter/GameOverPane.java
@@ -1,6 +1,7 @@
 package starter;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -28,8 +29,10 @@ public class GameOverPane extends GraphicsPane {
 	private int score;
 	private ArrayList<Integer> scores = new ArrayList<Integer>();
 	
-	String text;
-	
+	private GLabel topScores = new GLabel("HIGHEST SCORES",514,270);
+	private GLabel currentScore = new GLabel("CURRENT SCORE",515,140);
+	private GLabel scoreLabel = new GLabel("",605,200);
+	private GLabel gameOver = new GLabel("G  A  M  E  O  V  E  R",420,80);
 	
 	public GameOverPane(MainApplication app,int score) {
 		super();
@@ -37,69 +40,68 @@ public class GameOverPane extends GraphicsPane {
 		this.score = score;
 		mainMenu = new GButton("Main Menu", RIGHT_BOTTOM, BOTTOM, REG_BUTTON_WIDTH, REG_BUTTON_HEIGHT);
 		mainMenu.setFillColor(BUTTON_COLOR);
-		//readScore();
-		
+		topScores.setFont(new Font("Ariel", Font.PLAIN,30));
+		topScores.setColor(Color.BLUE);
+		currentScore.setFont(new Font("Ariel", Font.PLAIN,30));
+		currentScore.setColor(Color.BLUE);
+		scoreLabel.setFont(new Font("Ariel", Font.PLAIN,35));
+		scoreLabel.setLabel(Integer.toString(score));
+		gameOver.setFont(new Font("Ariel", Font.PLAIN,45));
+		gameOver.setColor(Color.black);
 	}
-	private void writeScore() {// writing of score to a text file.called in gameover().
+	private void writeScore() {// writing of score to a text file.
 		 try {
-	           // FileOutputStream outputStream = new FileOutputStream("../media/score.txt");
-	            //OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
 			 	File scoreFile = new File("../media/score.txt");
 			 	FileWriter fileWrite = new FileWriter(scoreFile,true);
 	            BufferedWriter bufferedWriter = new BufferedWriter(fileWrite);
 	           
-	         //   scores.add(score);
 	            System.out.println(Integer.toString(score));
 	            bufferedWriter.flush();
 	            bufferedWriter.newLine();
-	            bufferedWriter.write(Integer.toString(score));
-	            //bufferedWriter.append(Integer.toString(score));
-	            
-	          //  bufferedWriter.newLine();
-	             
+	            bufferedWriter.write(Integer.toString(score)); 
 	            bufferedWriter.close();
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
 		
 	}
-	private void readScore() {// reading of score from a text file.//called when collision.
+	private void readScore() {// reading of score from a text file.
 		try {
             FileReader reader = new FileReader("../media/score.txt");
             BufferedReader bufferedReader = new BufferedReader(reader);
  
             String line;
-            int i = 0;
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
                 scores.add(Integer.parseInt(line));
-              
-                
             }
-            Collections.sort(scores,Collections.reverseOrder());
-            while(i<5 && i < scores.size()) {
-            	  GLabel label = new GLabel(scores.get(i).toString(),500,40+(i*40));
-            	  program.add(label);
-            		
-            		i++;
-            }
+            Collections.sort(scores,Collections.reverseOrder());//Sorting scores with highest score being at the top.
+            scoresDisplaying();
             reader.close();
             
         } catch (IOException e) {
             e.printStackTrace();
         }
 	}
+	private void scoresDisplaying() {
+		 int i = 0;
+		 while(i<5 && i < scores.size()) {
+       	  GLabel label = new GLabel(scores.get(i).toString(),605,320+(i*50));
+       	  label.setFont(new Font("Ariel", Font.PLAIN,25));
+       	  program.add(label);
+       		
+       		i++;
+       }
+	}
 	@Override
 	public void showContents() {
 		program.add(mainMenu);
 		writeScore();
 		readScore();
-		
-		/*
-		for(GLabel score: last5scores) {
-		program.add(score);
-		}
-		*/
+		program.add(topScores);
+		program.add(currentScore);
+		program.add(scoreLabel);
+		program.add(gameOver);
 	}
 
 	@Override
